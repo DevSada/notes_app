cb5f92004c483824e342cedd151582de1164c88a
devsada <aleksander.petrenko@gmail.com>
2022-12-22 22:43:50 +0300

    start project

---
+++ notes_app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  notes_app
+//
+//  Created by Alexander Petrenko on 22.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
